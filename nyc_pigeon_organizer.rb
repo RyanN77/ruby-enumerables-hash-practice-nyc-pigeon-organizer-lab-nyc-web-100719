78b19fc7bf68a645c458b490f06a228af00f6bb4
adventurous-class-0665 <rngao97@gmail.com>
2019-09-24 20:04:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash_data = {}
+  data.each do |key, value|
+    value.each do |key1, value1|
+      i = 0
+      while i < value1.length
+      if hash_data.key?(value1[i]) == true
+        if hash_data[value1[i]].key?(key) == true
+          hash_data[value1[i]][key] << "#{key1}"
+        else
+        hash_data[value1[i]][key] = ["#{key1}"]
+        end
+      else
+        hash_data[value1[i]] = {key => ["#{key1}"]}
+      end
+        i += 1
+        end
+      end
+    end
+    print hash_data
+  return hash_data
 end
