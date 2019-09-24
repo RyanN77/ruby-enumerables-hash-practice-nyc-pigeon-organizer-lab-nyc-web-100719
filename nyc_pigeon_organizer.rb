16a0878bc198390fb54b26d5b7fffeeeca15b7d8
adventurous-class-0665 <rngao97@gmail.com>
2019-09-24 19:24:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |key1, value1|
       i = 0
-      print value1
-      while i < key1.length
+      while i < value1.length
       hash_data[value1[i]] = "hi" 
       
       
